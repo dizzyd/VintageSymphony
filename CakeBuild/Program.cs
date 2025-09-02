2ff8cd88210c3964eb422820b3b3e62ae936fd67
Daniel Reichelt <dr1400@web.de>
2025-09-02 17:45:42 +0200

    Update packaging logic: adjust version in zip filenames

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -76,6 +76,15 @@ public class BuildContext : FrostingContext
 		ModAssetsName = modAssetsInfo.ModID;
 	}
 	
+	public string AdjustVersionForFilename(string version)
+	{
+		if (TargetFramework == "net7.0")
+		{
+			return version + "-vs120";
+		}
+		return version;
+	}
+	
 	private string DetectTargetFramework()
 	{
 		try
@@ -227,9 +236,11 @@ public sealed class PackageModTask : FrostingTask<BuildContext>
 
 		// copy modinfo.json
 		context.CopyFile($"{projectDir}/modinfo.json", $"{buildDir}/modinfo.json");
+		context.EnsureDirectoryExists($"{buildDir}/assets/vintagesymphony/config/");
+		context.CopyFile($"{projectDir}/compatibility.json", $"{buildDir}/assets/vintagesymphony/config/compatibility.json");
 
 		// package mod
-		context.Zip(buildDir, $"{releasePath}/{context.ModName}_{context.ModVersion}.zip");
+		context.Zip(buildDir, $"{releasePath}/{context.ModName}_{context.AdjustVersionForFilename(context.ModVersion)}.zip");
 	}
 }
 
